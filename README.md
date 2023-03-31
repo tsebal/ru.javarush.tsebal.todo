1af0dff59d4258daf0bf76e8f2a3988b19b939ba
Tsebal <atsebal@gmail.com>
2023-03-31 19:01:51 +0300

    Added: README.md, Styles, todoapp.js Refactored: TaskController, tasks.html Fully implemented frontend and backend.

---
+++ README.md
@@ -0,0 +1,2 @@
+# ToDo Tasks by Tsebal v.0.1b
+The program is an online task list. You can add new tasks, edit and delete them, as well as set their status. Host it on a hosting site that supports Docker containers.
--- src/main/java/ru/javarush/todo/controller/TaskController.java
+++ src/main/java/ru/javarush/todo/controller/TaskController.java
@@ -16,6 +16,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static java.util.Objects.isNull;
 
@@ -35,13 +37,20 @@ public class TaskController {
                         @RequestParam(value = "limit", required = false, defaultValue = "10") int limit) {
         List<Task> tasks = taskService.findAll((page - 1) * limit, limit);
         model.addAttribute("tasks", tasks);
+        model.addAttribute("current_page", page);
+
+        int pagesTotal = (int) Math.ceil(1.0 * taskService.findAllCount() / limit);
+        if (pagesTotal > 1) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, pagesTotal).boxed().collect(Collectors.toList());
+            model.addAttribute("page_numbers", pageNumbers);
+        }
         return "tasks";
     }
 
     @PostMapping("/{id}")
     public String edit(Model model,
-                         @PathVariable Integer id,
-                         @RequestBody TaskDto taskDto) {
+                       @PathVariable Integer id,
+                       @RequestBody TaskDto taskDto) {
         if (isNull(id) || id <= 0) {
             logger.error("An incorrect task ID: " + id + " was sent.");
             throw new RuntimeException("Wrong ID.");
@@ -53,7 +62,7 @@ public class TaskController {
 
     @PostMapping("/")
     public String add(Model model,
-                    @RequestBody TaskDto taskDto) {
+                      @RequestBody TaskDto taskDto) {
         Task task = taskService.create(taskDto.getDescription(), taskDto.getStatus());
         return tasks(model, 1, 10);
     }
